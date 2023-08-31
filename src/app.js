e0095f8be9e0c12cf8b0fd9731226e17b25f2cd1
SusanTerrazo <renacer.02.10@gmail.com>
2023-08-30 22:08:10 -0500

    desafío websocket-handlebars

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 import express from 'express'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
 import productRouter from './routers/products.router.js'
-import cartRouter from './routers/carts.router.js'
-
 
 
 const app = express()
 
-app.use(express.json())
+//========================================setear Handlebars================================
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
 
-app.use('/api/products', productRouter)
-app.use('/api/carts', cartRouter)
+app.use(express.json())
+app.use(express.static('./src/public'))
+
+//===================================== rutas =============================================
+
+//Crear una vista “home.handlebars” la cual contenga una lista de todos los productos agregados hasta el momento
+app.use('/products', productRouter)
+
+
 
 const PORT = 8080
-app.listen(PORT, () => console.log(`Server up on http://localhost:${PORT}`)) 
+const httpServer = app.listen(PORT, () => console.log(`Server up on http://localhost:${PORT}`)) 
+const socketServer = new Server(httpServer)
---
+++ src/views/index.js
@@ -0,0 +1,1 @@
+const socketclient = io()
